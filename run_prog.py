fb5b3ae4327fcfde72002dcc836aa130f47e7ac4
basic-bgnr <devkotabidit@gmail.com>
2023-10-22 17:50:54 +0545

    modified hyper-parameter and print formatting

--- run_prog.py
+++ run_prog.py
@@ -4,17 +4,17 @@ import deeplearning.utils as utils
 
 
 data_set_location = "./mnist_dataset/"
-training_data_ratio = 0.1
-validation_data_ratio = 0.1
-testing_data_ratio = 0.1
+training_data_ratio = 1.0
+validation_data_ratio = 1.0 / 6.0
+testing_data_ratio = 0.10
 
 
 network_layers = [28 * 28, 30, 10]
-epochs = 6
+epochs = 30
 batch_size = 10
-eta = 0.5
-lamda = 0.5
-mu = 0.1
+eta = 2.0
+lamda = 0.9
+mu = 0.3
 
 training_data, validation_data, testing_data = utils.getTrainingValidationTestingData(
     data_set_location=data_set_location,
@@ -28,7 +28,7 @@ print(
 )
 
 
-print(f"{'Neural Network Initialized':<35}: {network_layers}")
+print(f"{'Neural Network Layers ([nodes])':<35}: {network_layers}")
 print(f"{'Hyper Parameters':<35}: {epochs=}, {batch_size=}, {eta=}, {lamda=}, {mu=}\n")
 
 net = ML.NeuralNetwork(network_layers)
